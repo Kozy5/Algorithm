ad1138fe0a0d6ba59d0fd5bab7340ce4a93028f7
Kojinwoo <96171308+Kozy5@users.noreply.github.com>
2024-03-15 21:51:56 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -0,0 +1,19 @@
+select PRICE_GROUP,PRODUCTS
+from
+(
+select case when price between 0     and 9999  then 0
+            when price between 10000 and 19999 then 10000
+            when price between 20000 and 29999 then 20000
+            when price between 30000 and 39999 then 30000
+            when price between 40000 and 49999 then 40000 
+            when price between 50000 and 59999 then 50000
+            when price between 60000 and 69999 then 60000
+            when price between 70000 and 79999 then 70000
+            when price between 80000 and 89999 then 80000
+            when price between 90000 and 99999 then 90000 end PRICE_GROUP,
+            count(product_id) PRODUCTS
+from product
+group by 1 
+) a
+where price_group
+order by price_group
