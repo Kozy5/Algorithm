e7c9e4dff69ef132628c03ea3de26f3237376ee9
Kojinwoo <96171308+Kozy5@users.noreply.github.com>
2024-04-23 01:01:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
@@ -1,10 +1,10 @@
 class Solution {
     fun solution(num: Int): Int {
         var answer = 0
-        var input = num
+        var input:Long = num.toLong()
         
-        while(input != 1){
-            if(input % 2 == 0){
+        while(input != 1.toLong()){
+            if(input % 2 == 0.toLong()){
                 input /= 2
                 answer += 1
             }else{
@@ -12,7 +12,7 @@ class Solution {
                 answer+= 1
             }
             
-            if(answer == 400){
+            if(answer == 500){
                 answer = -1
                 break
             }
