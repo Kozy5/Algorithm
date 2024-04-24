6a8b29a83a891d77201a9e15600523a4950a5a22
Kojinwoo <96171308+Kozy5@users.noreply.github.com>
2024-04-24 09:35:42 +0900

    [level 1] Title: 음양 더하기, Time: 0.36 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/README.md
+++ 프로그래머스/1/76501. 음양 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 음양 더하기 - 76501 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501?language=kotlin) 
 
 ### 성능 요약
 
-메모리: 60.3 MB, 시간: 0.33 ms
+메모리: 62.5 MB, 시간: 0.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 09:30:39
+2024년 04월 24일 09:35:42
 
 ### 문제 설명
 
--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.kt
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.kt
@@ -1,22 +1,22 @@
 class Solution {
     fun solution(absolutes: IntArray, signs: BooleanArray): Int {
         var answer: Int = 0
-        var realinputs = Array(absolutes.size,{i->i+1})
-        var changesigns = Array(signs.size,{i->i+1})   
-        signs.forEachIndexed{i,it->
-            if(it==true){
+        var realinputs = Array(signs.size,{i->i+1})
+        var changesigns = Array(signs.size,{i->i+1})
+        signs.forEachIndexed{ i,it ->
+            if(it == true){
                 changesigns[i] = 1
             }else {
-                changesigns[i]= -1
+                changesigns[i]=-1
             }
         }
-        absolutes.forEachIndexed{i,it->
-            realinputs[i] = it * changesigns[i]
+        absolutes.forEachIndexed{i,it ->
+            realinputs[i] = it*changesigns[i]
         }
         realinputs.forEach{
             answer += it
         }
-        
+      
         return answer
     }
 }
